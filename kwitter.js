dad11d4ae9885d3fb79a85bf5325ab41698968e2
doremonRudraksh <90757183+doremonRudraksh@users.noreply.github.com>
2021-12-04 11:03:18 +0530

    Delete kwitter.js

--- kwitter.js
+++
@@ -1,7 +0,0 @@
-function login(){
-    user_name = document.getElementById("name_input").Value;
-
-    localStorage.setItem("name_input", user_name);
-
-    window.location = "kwitter_room.html";
-}
